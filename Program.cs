1e47db5f4f517bf6bc8dc707a0473951536d618e
cycy111 <32603450+cycy111@users.noreply.github.com>
2018-10-13 10:35:27 +0800

    client code

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AbstractFactoryPattern.itf;
+using AbstractFactoryPattern.Model;
+namespace AbstractFactoryPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IKfcFactory kfc = new ChinaKfcFactory();
+            Customer customer = new Customer(kfc);
+            float hbTotal= customer.OrderHumburg(2);
+            float swTotal= customer.OrderKitchenwing(4);
+            Console.WriteLine("总价：{0}", hbTotal + swTotal);
+            
+            Console.ReadKey();
+        }
+    }
+}
